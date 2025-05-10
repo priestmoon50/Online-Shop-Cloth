d5e489e25a9160d29502391d851b2e8abdf211c1
Pezhman <33788011+priestmoon50@users.noreply.github.com>
2025-05-10 20:23:05 +0330

    Update page.tsx

--- FrontEnd/src/app/support/page.tsx
+++ FrontEnd/src/app/support/page.tsx
@@ -24,7 +24,7 @@ export default function SupportPage() {
         className={`${styles.navbar} ${isMobile && active ? styles.active : ""}`}
         onClick={handleClick}
       >
-        Social
+        Click
         <ul className={styles.menu}>
           <li><a href="#"><Facebook style={{ color: "#1877F2" }} fontSize="large" /></a></li>
           <li><a href="https://www.instagram.com/mopastyle_/" target="_blank" rel="noopener noreferrer">
