84813d7460024785d56582fb1ad60edd5ff14093
Pezhman <33788011+priestmoon50@users.noreply.github.com>
2025-05-17 03:30:06 +0330

    Update Flash

--- FrontEnd/src/components/ProductDetails/ProductImages.tsx
+++ FrontEnd/src/components/ProductDetails/ProductImages.tsx
@@ -1,22 +1,56 @@
+'use client';
+
 import { FC } from 'react';
 import { Box } from '@mui/material';
 import Image from 'next/image';
-import Slider from "react-slick";
+import Slider from 'react-slick';
+import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material';
 import { ProductImagesProps } from '@/data/types';
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
+import styles from './ProductImages.module.css';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+interface ArrowProps {
+  onClick?: () => void;
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+
+const CustomPrevArrow: FC<ArrowProps> = ({ onClick }) => (
+  <div
+    className={`${styles.customArrow} ${styles.leftArrow}`}
+    onClick={onClick}
+  >
+    <ArrowBackIos className={styles.arrowIcon} />
+  </div>
+);
+
+const CustomNextArrow: FC<ArrowProps> = ({ onClick }) => (
+  <div
+    className={`${styles.customArrow} ${styles.rightArrow}`}
+    onClick={onClick}
+  >
+    <ArrowForwardIos className={styles.arrowIcon} />
+  </div>
+);
+
+
 
 const ProductImages: FC<ProductImagesProps> = ({ images }) => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    adaptiveHeight: false,
-    swipe: true,      
-    touchMove: true, 
-  };
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  swipe: true,
+  touchMove: true,
+  nextArrow: <CustomNextArrow />,
+  prevArrow: <CustomPrevArrow />,
+};
+
 
   return (
     <Box
@@ -27,12 +61,14 @@ const ProductImages: FC<ProductImagesProps> = ({ images }) => {
         mb: { xs: 4, md: 0 },
       }}
     >
-      <Box
-        sx={{
-          width: { xs: '95vw', sm: '90vw', md: '500px' },
-          maxWidth: '135%',
-        }}
-      >
+<Box
+  sx={{
+    width: { xs: '95vw', sm: '90vw', md: '500px' },
+    maxWidth: '135%',
+    position: 'relative', // بسیار مهم برای موقعیت‌یابی فلش‌ها
+  }}
+>
+
         <Slider {...settings}>
           {images.map((image, index) => (
             <Box key={index}>
